f0baf75b101b23e318fb96b29f562e95ee52b5c7
MTTA96 <mtta200396@gmail.com>
2017-08-28 23:26:14 +0700

    Khởi tạo project

---
+++ Code/app/src/main/java/com/eways/elearning/Presenter/View/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eways.elearning.Presenter.View.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eways.elearning.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

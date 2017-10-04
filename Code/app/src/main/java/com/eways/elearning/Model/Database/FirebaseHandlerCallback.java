4dfcf135e68a1fe2400e3d23f8002a4fe27565ef
Quang Trí <quangtri_1996@yahoo.com>
2017-10-04 22:16:48 +0700

    Update UI + Build class firebaseHandler

---
+++ Code/app/src/main/java/com/eways/elearning/Model/Database/FirebaseHandlerCallback.java
@@ -0,0 +1,10 @@
+package com.eways.elearning.Model.Database;
+
+/**
+ * Created by Quang Tri on 04/10/2017.
+ */
+
+public interface FirebaseHandlerCallback {
+    //Truyền dữ liệu
+    void TransData(String data);
+}
--- Code/app/src/main/java/com/eways/elearning/View/Activity/Activity/MainActivity.java
+++ Code/app/src/main/java/com/eways/elearning/View/Activity/Activity/MainActivity.java
@@ -2,17 +2,26 @@ package com.eways.elearning.View.Activity.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.eways.elearning.Model.Database.FireBaseHandler;
+import com.eways.elearning.Model.Database.FirebaseHandlerCallback;
 import com.eways.elearning.R;
 import com.eways.elearning.View.Fragment.DangNhap.DangNhapFragment;
 import com.facebook.FacebookSdk;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FirebaseHandlerCallback{
+    FireBaseHandler fireBaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().replace(R.id.LoMain,new DangNhapFragment()).commit();
+
+    }
+    //FirebaseHandlerCallback trả dữ liệu
+    @Override
+    public void TransData(String data) {
     }
 }

3b5d7c8c8ea4431b4e30284df64af32f8dbff519
MTTA96 <mtta200396@gmail.com>
2017-11-05 10:31:44 +0700

    Add action for nav menu

--- Code/app/src/main/java/com/eways/elearning/View/Activity/MainActivity.java
+++ Code/app/src/main/java/com/eways/elearning/View/Activity/MainActivity.java
@@ -1,18 +1,21 @@
 package com.eways.elearning.View.Activity;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.eways.elearning.Model.FragmentHandler;
 import com.eways.elearning.R;
 import com.eways.elearning.View.Fragment.Home.HomeFragment;
 import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private FragmentHandler fragmentHandler;
 
     @Override
@@ -21,13 +24,18 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
 
-        //Thay Actionbar mặc định bằng toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_tool_bar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        //Set sự kiện
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Thay Actionbar mặc định bằng toolbar
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         //Đồng bộ toolbar và slide menu
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, myToolbar, 0, 0);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         drawer.addDrawerListener(toggle);
@@ -36,5 +44,14 @@ public class MainActivity extends AppCompatActivity{
 
         fragmentHandler = new FragmentHandler(this, getSupportFragmentManager());
         fragmentHandler.ChuyenFragment(new HomeFragment(), false, null);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.act_quan_ly_tai_khoan){
+            fragmentHandler.ChuyenFragment(new );
+        }
+        return false;
     }
 }

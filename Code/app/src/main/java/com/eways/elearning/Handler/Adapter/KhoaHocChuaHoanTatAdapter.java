01dcfd6a35e08d682e45703a75a1b74216fa297f
MTTA96 <mtta200396@gmail.com>
2017-11-16 19:26:19 +0700

    Giao diện tạo khóa học, chặn code KhoaHocChuaHoanTatAdapter (line: 50-78)

--- Code/app/src/main/java/com/eways/elearning/Handler/Adapter/KhoaHocChuaHoanTatAdapter.java
+++ Code/app/src/main/java/com/eways/elearning/Handler/Adapter/KhoaHocChuaHoanTatAdapter.java
@@ -47,35 +47,35 @@ public class KhoaHocChuaHoanTatAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(mLayout, null);
-
-        ImageView imvAvatar = (ImageView) convertView.findViewById(R.id.imvAvatar);
-//        imvAvatar.setImageURI(url);
-//        lấy UserID của người đăng khóa học => lấy link avatar trong tài khoản
-
-        RatingBar rbBaiDang = (RatingBar) convertView.findViewById(R.id.rbBaiDang);
-//        rbBaiDang.setRating( );
-//        lấy UserID của người đăng khóa học => lấy rating trong tài khoản
-
-        TextView tvTenNguoiDang = (TextView) convertView.findViewById(R.id.tvTenNguoiDang);
-        tvTenNguoiDang.setText(khoaHocChuaHoanTatList.get(position).getNguoiDang());
-
-        TextView tvBuoiHoc = (TextView) convertView.findViewById(R.id.tvBuoiHoc);
-        tvTenNguoiDang.setText(khoaHocChuaHoanTatList.get(position).getSoBuoiHoc());
-
-        TextView tvMonHoc = (TextView) convertView.findViewById(R.id.tvMonHoc);
-//        tvTenNguoiDang.setText(Arrays.toString(khoaHocChuaHoanTatList.get(position).getMon().toArray()));
-//        non-recommended! Không chắc đúng hay không? Với lại hiện thị kiểu này hông đẹp
-//        ArrayList<String> listMon = khoaHocChuaHoanTatList.get(position).getMon();
-//        String x = null;
-//        for(int i =0;i<listMon.size();i++)
-//        {
-//            x += listMon.get(i);
-//        }
-        tvMonHoc.setText("a");
-
-        TextView tvXemThem = (TextView) convertView.findViewById(R.id.tvXemThem);
+//        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        convertView = inflater.inflate(mLayout, null);
+//
+//        ImageView imvAvatar = (ImageView) convertView.findViewById(R.id.imvAvatar);
+////        imvAvatar.setImageURI(url);
+////        lấy UserID của người đăng khóa học => lấy link avatar trong tài khoản
+//
+//        RatingBar rbBaiDang = (RatingBar) convertView.findViewById(R.id.rbBaiDang);
+////        rbBaiDang.setRating( );
+////        lấy UserID của người đăng khóa học => lấy rating trong tài khoản
+//
+//        TextView tvTenNguoiDang = (TextView) convertView.findViewById(R.id.tvTenNguoiDang);
+//        tvTenNguoiDang.setText(khoaHocChuaHoanTatList.get(position).getNguoiDang());
+//
+//        TextView tvBuoiHoc = (TextView) convertView.findViewById(R.id.tvBuoiHoc);
+//        tvTenNguoiDang.setText(khoaHocChuaHoanTatList.get(position).getSoBuoiHoc());
+//
+//        TextView tvMonHoc = (TextView) convertView.findViewById(R.id.tvMonHoc);
+////        tvTenNguoiDang.setText(Arrays.toString(khoaHocChuaHoanTatList.get(position).getMon().toArray()));
+////        non-recommended! Không chắc đúng hay không? Với lại hiện thị kiểu này hông đẹp
+////        ArrayList<String> listMon = khoaHocChuaHoanTatList.get(position).getMon();
+////        String x = null;
+////        for(int i =0;i<listMon.size();i++)
+////        {
+////            x += listMon.get(i);
+////        }
+//        tvMonHoc.setText("a");
+//
+//        TextView tvXemThem = (TextView) convertView.findViewById(R.id.tvXemThem);
 
         return convertView;
     }
--- Code/app/src/main/java/com/eways/elearning/Handler/Adapter/KhoaHocDaHoanTatAdapter.java
+++ Code/app/src/main/java/com/eways/elearning/Handler/Adapter/KhoaHocDaHoanTatAdapter.java
@@ -48,28 +48,28 @@ public class KhoaHocDaHoanTatAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(mLayout, null);
-
-        ImageView imvAvatar = (ImageView) convertView.findViewById(R.id.imvAvatar);
-//        imvAvatar.setImageURI(url);
-//        lấy UserID của người đăng khóa học => lấy link avatar trong tài khoản
-
-        RatingBar rbBaiDang = (RatingBar) convertView.findViewById(R.id.rbBaiDang);
-//        rbBaiDang.setRating( );
-//        lấy UserID của người đăng khóa học => lấy rating trong tài khoản
-
-        TextView tvTenNguoiDang = (TextView) convertView.findViewById(R.id.tvTenNguoiDang);
-        tvTenNguoiDang.setText(khoaHocDaHoanTatList.get(position).getNguoiDang());
-
-        TextView tvBuoiHoc = (TextView) convertView.findViewById(R.id.tvBuoiHoc);
-        tvTenNguoiDang.setText(khoaHocDaHoanTatList.get(position).getSoBuoiHoc());
-
-        TextView tvMonHoc = (TextView) convertView.findViewById(R.id.tvMonHoc);
-        tvTenNguoiDang.setText(Arrays.toString(khoaHocDaHoanTatList.get(position).getMon().toArray()));
-//        non-recommended! Không chắc đúng hay không? Với lại hiện thị kiểu này hông đẹp
-
-        TextView tvXemThem = (TextView) convertView.findViewById(R.id.tvXemThem);
+//        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        convertView = inflater.inflate(mLayout, null);
+//
+//        ImageView imvAvatar = (ImageView) convertView.findViewById(R.id.imvAvatar);
+////        imvAvatar.setImageURI(url);
+////        lấy UserID của người đăng khóa học => lấy link avatar trong tài khoản
+//
+//        RatingBar rbBaiDang = (RatingBar) convertView.findViewById(R.id.rbBaiDang);
+////        rbBaiDang.setRating( );
+////        lấy UserID của người đăng khóa học => lấy rating trong tài khoản
+//
+//        TextView tvTenNguoiDang = (TextView) convertView.findViewById(R.id.tvTenNguoiDang);
+//        tvTenNguoiDang.setText(khoaHocDaHoanTatList.get(position).getNguoiDang());
+//
+//        TextView tvBuoiHoc = (TextView) convertView.findViewById(R.id.tvBuoiHoc);
+//        tvTenNguoiDang.setText(khoaHocDaHoanTatList.get(position).getSoBuoiHoc());
+//
+//        TextView tvMonHoc = (TextView) convertView.findViewById(R.id.tvMonHoc);
+//        tvTenNguoiDang.setText(Arrays.toString(khoaHocDaHoanTatList.get(position).getMon().toArray()));
+////        non-recommended! Không chắc đúng hay không? Với lại hiện thị kiểu này hông đẹp
+//
+//        TextView tvXemThem = (TextView) convertView.findViewById(R.id.tvXemThem);
 
         return convertView;
     }

297109a36261ab14b7b331c5d17073883c76960b
PhatMap <yowindii@gmail.com>
2017-12-03 21:50:06 +0700

    LinhVuc model + presenter

---
+++ Code/app/src/main/java/com/eways/elearning/Presenter/LinhVuc/LinhVucPresenterImp.java
@@ -0,0 +1,14 @@
+package com.eways.elearning.Presenter.LinhVuc;
+
+import com.eways.elearning.DataModel.LinhVuc.LinhVuc;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yowin on 03/12/2017.
+ */
+
+public interface LinhVucPresenterImp {
+    public void yeuCauLinhVuc();
+    public void nhanListLinhVuc(ArrayList<LinhVuc> linhVucs);
+}

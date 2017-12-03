2eceb8516d7637fe7298068efee97e790aebb607
PhatMap <yowindii@gmail.com>
2017-12-03 14:21:25 +0700

    Model Linh vuc + Mon

--- Code/app/src/main/java/com/eways/elearning/View/Fragment/Home/NewHomeFragment.java
+++ Code/app/src/main/java/com/eways/elearning/View/Fragment/Home/NewHomeFragment.java
@@ -3,29 +3,18 @@ package com.eways.elearning.View.Fragment.Home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.support.v4.view.ViewPager;
 import android.view.ViewGroup;
-import android.support.design.widget.TabLayout;
 
-import com.eways.elearning.DataModel.KhoaHoc.CustomModelKhoaHoc;
-import com.eways.elearning.Handler.Adapter.KhoaHoc.DanhSachKhoaHocHomeAdapter;
 import com.eways.elearning.Handler.Adapter.ViewPagerAdapter;
-import com.eways.elearning.Handler.FragmentHandler;
-import com.eways.elearning.Handler.ImageHandler;
-import com.eways.elearning.Model.Database.SharedPreferencesHandler;
-import com.eways.elearning.Presenter.Home.NewHomeFragmentPresenter;
 import com.eways.elearning.R;
-import com.eways.elearning.Util.SupportKeysList;
 import com.eways.elearning.View.Fragment.Home.HomeTimGiaSu.HomeTimGiaSuFragment;
 import com.eways.elearning.View.Fragment.Home.HomeTimHocVien.HomeTimHocVienFragment;
-import com.eways.elearning.View.Fragment.ListKhoaHoc.ListKhoaHocFragment;
-
-import java.util.ArrayList;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,7 +23,7 @@ public class NewHomeFragment extends Fragment {
 
     private final String titleTab1 = "Tìm gia sư";
     private final String titleTab2 = "Tìm học viên";
-
+    DatabaseReference mData = FirebaseDatabase.getInstance().getReference();
     public NewHomeFragment() {
         // Required empty public constructor
     }

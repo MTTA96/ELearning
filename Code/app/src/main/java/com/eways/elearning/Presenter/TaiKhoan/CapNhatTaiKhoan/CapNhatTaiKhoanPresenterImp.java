01e0d237ea91c50a73d5a8d5e0cd1ef0a5c99c38
Quang Tri <hangquangtri3111996@gmail.com>
2018-01-08 21:53:45 +0700

    Set TaiKhoanGiaSu

--- Code/app/src/main/java/com/eways/elearning/Presenter/TaiKhoan/CapNhatTaiKhoan/CapNhatTaiKhoanPresenterImp.java
+++ Code/app/src/main/java/com/eways/elearning/Presenter/TaiKhoan/CapNhatTaiKhoan/CapNhatTaiKhoanPresenterImp.java
@@ -12,4 +12,6 @@ import com.eways.elearning.DataModel.TaiKhoan.TaiKhoan;
 public interface CapNhatTaiKhoanPresenterImp {
     public void NhanDataUpdate (TaiKhoan taiKhoan, Activity activity, ImageView ivTaiLieuXacMinh_MT,ImageView ivTaiLieuXacMinh_MS,ImageView ivAvarta);
     public void KetQuaCapNhat(String ketquacapnhat,TaiKhoan taiKhoan,Activity activity);
+    public void NhanDataCapNhatTaiKhoanGiaSu(String idUser,Activity activity);
+    public void KetQuaCapNhatTaiKhoanGiaSu(String ketQua);
 }

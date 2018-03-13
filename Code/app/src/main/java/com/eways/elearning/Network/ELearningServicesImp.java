582d274c415bdfce0b0aadb0a32c3f8c53608ae7
Tuan Anh <mtta200396@gmail.com>
2018-03-13 13:55:25 +0700

    Add retrofit and gson

---
+++ Code/app/src/main/java/com/eways/elearning/Network/ELearningServicesImp.java
@@ -0,0 +1,12 @@
+package com.eways.elearning.Network;
+
+import retrofit2.http.POST;
+
+/**
+ * Created by zzzzz on 3/13/2018.
+ */
+
+public interface ELearningServicesImp {
+//    @POST("/market/ETH/AUD/tick?since=now")
+//    Call<User> getData();
+}
---
+++ Code/app/src/main/java/com/eways/elearning/Util/ApiUtils.java
@@ -0,0 +1,13 @@
+package com.eways.elearning.Util;
+
+/**
+ * Created by zzzzz on 3/13/2018.
+ */
+
+public class ApiUtils {
+    public static final String BASE_URL = "ewayslearn.000webhostapp.com/";
+//
+//    public static BtcmarketServiceImp getBtcmarketService() {
+//        return RetrofitClient.getClient(BTCMARKET_BASE_URL).create(BtcmarketServiceImp.class);
+//    }
+}
